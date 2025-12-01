9f8533b6ffe1f5bbc8b3139e3cf22f10aa8ccbff
waynee95 <larsericmarquardt@mail.de>
2025-12-01 17:34:47 +0100

    Finished Day 01, Part 2

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -29,11 +29,19 @@ rotate val (Rotation dir rot) = case dir of
     R -> (val + rot) `mod` 100
 
 part1 :: [Rotation] -> Int
-part1 rotations = length . filter (== 0) $ scanl rotate 50 rotations
+part1 = length . filter (== 0) . scanl rotate 50
+
+part2 :: [Rotation] -> Int
+part2 = part1 . concat . map step
+  where
+    step (Rotation L n) = replicate n (Rotation L 1)
+    step (Rotation R n) = replicate n (Rotation R 1)
 
 main :: IO ()
 main = do
     input <- fst . last . readP_to_S parseRotations <$> readFile "input.txt"
     printf "Part 1: %d\n" (part1 input)
+    printf "Part 2: %d\n" (part2 input)
 
 -- Part 1: 1055
+-- Part 2: 6386
